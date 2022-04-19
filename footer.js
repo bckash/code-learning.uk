b2019cca8a053c48344f25aaa8b2de4595c63cdd
bart c <kaszikasz@wp.pl>
2022-04-19 11:08:32 +0100

    upload before merging

--- footer.js
+++ footer.js
@@ -1,7 +1,11 @@
 
 //footer
 
-const foot =  `<img class="tag" src="img/sw.png" alt="">
-                <p class="p22">| 22</p>`;
+const foot =  `
+
+                <p class="p22">| 22</p>
+                `;
 
 export default foot;
+
+{/* <img class="tag" src="img/sw.png" alt=""> */}
